a2f6765c48b1af707db9850139e3d1072b57898e
JinXin <741799837@qq.com>
2024-08-04 22:17:17 +0800

    【角色系统】 新建高级动画插件SimpleAdvancedAnimatino，暂时用于制作FootIK功能

---
+++ Plugins/SimpleAdvancedAnimation/Source/SimpleAdvancedAnimation/Private/SimpleAdvancedAnimationBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimpleAdvancedAnimationBPLibrary.h"
+#include "SimpleAdvancedAnimation.h"
+
+USimpleAdvancedAnimationBPLibrary::USimpleAdvancedAnimationBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float USimpleAdvancedAnimationBPLibrary::SimpleAdvancedAnimationSampleFunction(float Param)
+{
+	return -1;
+}
+
