09b41492cb44f69fbda9ea87501892029e0271c0
JinXin <741799837@qq.com>
2024-08-08 10:42:16 +0800

    【飞行系统】 新建飞行组件FlyComponent 新建飞行动画MMORPGFlyAnimInstance 新建飞行动画蓝图SK_Tubaki_AnimFlyBP 修改PlayerAnimInstance,测试整合多个动画Graph

---
+++ Source/MMORPG/Core/Game/Animation/Component/MMORPGFlyAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../Instance/Core/MMORPGAnimInstanceBase.h"
+#include "MMORPGFlyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MMORPG_API UMMORPGFlyAnimInstance : public UMMORPGAnimInstanceBase
+{
+	GENERATED_BODY()
+	
+};
